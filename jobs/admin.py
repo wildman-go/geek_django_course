5dd426d07e21f782edbc4641b4e3dd7b47d1b320
wildman <962894047@qq.com>
2021-04-17 15:39:08 +0800

    在职位列表页显示职位详情

--- jobs/admin.py
+++ jobs/admin.py
@@ -1,6 +1,11 @@
 from django.contrib import admin
 from .models import Job
 
+
 # Register your models here.
 
-admin.site.register(Job)
+class JobAdmin(admin.ModelAdmin):
+    list_display = ['job_name', 'job_type', 'job_city', 'creator', 'created_date', 'modified_date']
+
+
+admin.site.register(Job, JobAdmin)
