40257a1b430cb24c507ba65f727ff5202928e26e
wildman <962894047@qq.com>
2021-04-18 18:10:19 +0800

    在简历列表页，增加功能:批量将简历转移到面试流程

--- jobs/admin.py
+++ jobs/admin.py
@@ -1,5 +1,25 @@
 from django.contrib import admin
 from .models import Job, Resume
+from interview.models import Candidate
+from django.contrib import messages
+from datetime import datetime
+
+
+def enter_interview_process(modeladmin, request, queryset):
+    candidate_names = ""
+    for query in queryset:
+        candidate = Candidate()
+        # 把query对象里面的所有属性拷贝到candidate对象中
+        candidate.__dict__.update(query.__dict__)
+        candidate.created_date = datetime.now()
+        candidate.modified_date = datetime.now()
+        candidate_names = candidate.username + ',' + candidate_names
+        candidate.creator = request.user.username
+        candidate.save()
+    messages.add_message(request, messages.INFO, '候选人: %s 已成功进入面试流程' % (candidate_names))
+
+
+enter_interview_process.short_description = '进入面试流程'
 
 
 # Register your models here.
@@ -18,6 +38,7 @@ class ResumeAdmin(admin.ModelAdmin):
                     'created_date']
 
     readonly_fields = ['created_date', 'modified_date', 'applicant']
+    actions = [enter_interview_process]
 
     fieldsets = (
         (None,
