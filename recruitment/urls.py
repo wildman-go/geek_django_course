033019ebc5c49211b08d556fa938a552ea3bf6c4
wildman <962894047@qq.com>
2021-04-18 12:22:25 +0800

    安装django-grappelli，切换admin的页面主题

--- recruitment/urls.py
+++ recruitment/urls.py
@@ -14,12 +14,14 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
+import grappelli
 from django.urls import path, include
 from django.conf.urls import url
 from django.utils.translation import gettext as _
 
 urlpatterns = [
     url(r"^", include("jobs.urls")),
+    path('grappelli/', include('grappelli.urls')),
     path('admin/', admin.site.urls),
 ]
 
