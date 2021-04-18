b0ab80b789df25d2d32a43e4698d5de59734474b
wildman <962894047@qq.com>
2021-04-18 16:21:52 +0800

    fix:职位列表页无法识别用户是否已登录

--- jobs/views.py
+++ jobs/views.py
@@ -10,12 +10,11 @@ from .models import JobTypes, Cities
 
 def joblist(request):
     job_list = Job.objects.order_by('job_type')
-    template = loader.get_template('job_list.html')
     context = {'job_list': job_list}
     for job in job_list:
         job.type_name = JobTypes[job.job_type][1]
         job.city_name = Cities[job.job_city][1]
-    return HttpResponse(template.render(context))
+    return render(request, 'job_list.html', context=context)
 
 
 def detail(request, job_id):
