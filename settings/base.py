f753e40506b4871e19261153e740f94ee1bc4e2f
wildman <962894047@qq.com>
2021-04-18 21:01:25 +0800

    自定义一个中间件，记录每一个请求的响应耗时，并打印log到性能日志

--- settings/base.py
+++ settings/base.py
@@ -48,6 +48,7 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
+    'interview.performance.performance_logger_middleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -144,6 +145,12 @@ LOGGING = {
             'formatter': 'simple',
             'filename': os.path.join(os.path.dirname(BASE_DIR), 'recruitment.admin.log')
         },
+        'performance': {
+            'class': 'logging.FileHandler',
+            'formatter': 'simple',
+            'filename': os.path.join(os.path.dirname(BASE_DIR), 'recruitment.performance.log'),
+        },
+
     },
     'root': {
         'handlers': ['console', 'file'],
@@ -153,6 +160,11 @@ LOGGING = {
         'django_python3': {
             'handlers': ['console', 'file'],
             'level': 'DEBUG',
+        },
+        'interview.performance': {
+            'handlers': ['console', 'performance'],
+            'level': 'INFO',
+            'propagate': False,
         }
     }
 }
