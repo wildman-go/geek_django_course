f5dfdf8de6e8ff4517aa7b8484763ecf07c4dd67
wildman <962894047@qq.com>
2021-04-18 21:36:49 +0800

    用django自带的gettext_lazy修改models文件和templates文件，实现多语言

--- interview/models.py
+++ interview/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import User
+from django.utils.translation import gettext_lazy as _
 
 # 第一轮面试结果
 FIRST_INTERVIEW_RESULT_TYPE = [('建议复试', '建议复试'), ('待定', '待定'), ('放弃', '放弃'), ]
@@ -16,92 +17,92 @@ DEGREE_TYPE = [('本科', '本科'), ('硕士', '硕士'), ('博士', '博士')]
 
 class Candidate(models.Model):
     # 基础信息
-    userid = models.IntegerField(unique=True, blank=True, null=True, verbose_name=r'应聘者ID')
-    username = models.CharField(max_length=135, verbose_name=r'姓名')
-    city = models.CharField(max_length=135, verbose_name=r'城市')
-    phone = models.CharField(max_length=135, verbose_name=r'手机号码')
-    email = models.CharField(max_length=135, blank=True, verbose_name=r'邮箱地址')
-    apply_position = models.CharField(max_length=135, blank=True, verbose_name=r'应聘职位')
-    born_address = models.CharField(max_length=135, blank=True, verbose_name=r'生源地')
-    gender = models.CharField(max_length=135, blank=True, verbose_name=r'性别')
-    candidate_remark = models.CharField(max_length=135, blank=True, verbose_name=r'候选人备注信息')
+    userid = models.IntegerField(unique=True, blank=True, null=True, verbose_name=_('应聘者ID'))
+    username = models.CharField(max_length=135, verbose_name=_('姓名'))
+    city = models.CharField(max_length=135, verbose_name=_('城市'))
+    phone = models.CharField(max_length=135, verbose_name=_('手机号码'))
+    email = models.CharField(max_length=135, blank=True, verbose_name=_('邮箱地址'))
+    apply_position = models.CharField(max_length=135, blank=True, verbose_name=_('应聘职位'))
+    born_address = models.CharField(max_length=135, blank=True, verbose_name=_('生源地'))
+    gender = models.CharField(max_length=135, blank=True, verbose_name=_('性别'))
+    candidate_remark = models.CharField(max_length=135, blank=True, verbose_name=_('候选人备注信息'))
 
     # 学校与学历信息
-    bachelor_school = models.CharField(max_length=135, blank=True, verbose_name=r'本科学校')
-    master_school = models.CharField(max_length=135, blank=True, verbose_name=r'研究生学校')
-    doctor_school = models.CharField(max_length=135, blank=True, verbose_name=r'博士生学校')
-    major = models.CharField(max_length=135, blank=True, verbose_name=r'专业')
-    degree = models.CharField(max_length=135, choices=DEGREE_TYPE, blank=True, verbose_name=r'学历')
+    bachelor_school = models.CharField(max_length=135, blank=True, verbose_name=_('本科学校'))
+    master_school = models.CharField(max_length=135, blank=True, verbose_name=_('研究生学校'))
+    doctor_school = models.CharField(max_length=135, blank=True, verbose_name=_('博士生学校'))
+    major = models.CharField(max_length=135, blank=True, verbose_name=_('专业'))
+    degree = models.CharField(max_length=135, choices=DEGREE_TYPE, blank=True, verbose_name=_('学历'))
 
     # 综合能力测评成绩，笔试测评成绩
     test_score_of_general_ability = models.DecimalField(decimal_places=1, null=True, max_digits=3, blank=True,
-                                                        verbose_name=r'综合能力测评成绩')
-    paper_score = models.DecimalField(decimal_places=1, null=True, max_digits=3, blank=True, verbose_name=r'笔试成绩')
+                                                        verbose_name=_('综合能力测评成绩'))
+    paper_score = models.DecimalField(decimal_places=1, null=True, max_digits=3, blank=True, verbose_name=_('笔试成绩'))
 
     # 第一轮面试记录
-    first_score = models.DecimalField(decimal_places=1, null=True, max_digits=2, blank=True, verbose_name=r'初试分',
-                                      help_text=r'1-5分，极优秀:>=4.5，优秀:4-4.4，良好:3.5-3.9,一般:3-3.4,较差:<3')
+    first_score = models.DecimalField(decimal_places=1, null=True, max_digits=2, blank=True, verbose_name=_('初试分'),
+                                      help_text=_('1-5分，极优秀:>=4.5，优秀:4-4.4，良好:3.5-3.9,一般:3-3.4,较差:<3'))
     first_learning_ability = models.DecimalField(decimal_places=1, null=True, max_digits=2, blank=True,
-                                                 verbose_name=r'学习能力得分')
+                                                 verbose_name=_('学习能力得分'))
     first_professional_competency = models.DecimalField(decimal_places=1, null=True, max_digits=2, blank=True,
-                                                        verbose_name=r'专业能力得分')
-    first_advantage = models.TextField(max_length=1024, blank=True, verbose_name=r'优势')
-    first_disadvantage = models.TextField(max_length=1024, blank=True, verbose_name=r'顾虑和不足')
+                                                        verbose_name=_('专业能力得分'))
+    first_advantage = models.TextField(max_length=1024, blank=True, verbose_name=_('优势'))
+    first_disadvantage = models.TextField(max_length=1024, blank=True, verbose_name=_('顾虑和不足'))
     first_result = models.CharField(max_length=256,
-                                    choices=FIRST_INTERVIEW_RESULT_TYPE, blank=True, verbose_name=r'初试结果')
-    first_recommend_position = models.CharField(max_length=256, blank=True, verbose_name=r'推荐部门')
+                                    choices=FIRST_INTERVIEW_RESULT_TYPE, blank=True, verbose_name=_('初试结果'))
+    first_recommend_position = models.CharField(max_length=256, blank=True, verbose_name=_('推荐部门'))
     first_interviewer_user = models.ForeignKey(User, related_name=r'first_interviewer_user', blank=True, null=True,
-                                               on_delete=models.CASCADE, verbose_name=r'一面面试官')
-    first_remark = models.CharField(max_length=135, blank=True, verbose_name=r'初试备注')
+                                               on_delete=models.CASCADE, verbose_name=_('一面面试官'))
+    first_remark = models.CharField(max_length=135, blank=True, verbose_name=_('初试备注'))
 
     # 第二轮面试记录
-    second_score = models.DecimalField(decimal_places=1, null=True, max_digits=2, blank=True, verbose_name=r'专业复试得分',
-                                       help_text=r'1-5分，极优秀:>=4.5，优秀:4-4.4，良好:3.5-3.9,一般:3-3.4,较差:<3')
+    second_score = models.DecimalField(decimal_places=1, null=True, max_digits=2, blank=True, verbose_name=_('专业复试得分'),
+                                       help_text=_('1-5分，极优秀:>=4.5，优秀:4-4.4，良好:3.5-3.9,一般:3-3.4,较差:<3'))
     second_learning_ability = models.DecimalField(decimal_places=1, null=True, max_digits=2, blank=True,
-                                                  verbose_name=r'学习能力得分')
+                                                  verbose_name=_('学习能力得分'))
     second_professional_competency = models.DecimalField(decimal_places=1, null=True, max_digits=2, blank=True,
-                                                         verbose_name=r'专业能力得分')
+                                                         verbose_name=_('专业能力得分'))
     second_pursue_of_excellence = models.DecimalField(decimal_places=1, null=True, max_digits=2, blank=True,
-                                                      verbose_name=r'追求卓越得分')
+                                                      verbose_name=_('追求卓越得分'))
     second_communication_ability = models.DecimalField(decimal_places=1, null=True, max_digits=2, blank=True,
-                                                       verbose_name=r'沟通能力得分')
+                                                       verbose_name=_('沟通能力得分'))
     second_pressure_score = models.DecimalField(decimal_places=1, null=True, max_digits=2, blank=True,
-                                                verbose_name=r'抗压能力得分')
-    second_advantage = models.TextField(max_length=1024, blank=True, verbose_name=r'优势')
-    second_disadvantage = models.TextField(max_length=1024, blank=True, verbose_name=r'顾虑和不足')
+                                                verbose_name=_('抗压能力得分'))
+    second_advantage = models.TextField(max_length=1024, blank=True, verbose_name=_('优势'))
+    second_disadvantage = models.TextField(max_length=1024, blank=True, verbose_name=_('顾虑和不足'))
     second_result = models.CharField(max_length=256,
-                                     choices=INTERVIEW_RESULT_TYPE, blank=True, verbose_name=r'专业复试结果')
-    second_recommend_position = models.CharField(max_length=256, blank=True, verbose_name=r'建议方向或推荐部门')
+                                     choices=INTERVIEW_RESULT_TYPE, blank=True, verbose_name=_('专业复试结果'))
+    second_recommend_position = models.CharField(max_length=256, blank=True, verbose_name=_('建议方向或推荐部门'))
     second_interviewer_user = models.ForeignKey(User, blank=True, null=True, on_delete=models.CASCADE,
-                                                related_name=r'second_interviewer_user', verbose_name=r'二面面试官')
-    second_remark = models.CharField(max_length=135, blank=True, verbose_name=r'专业复试备注')
+                                                related_name=r'second_interviewer_user', verbose_name=_('二面面试官'))
+    second_remark = models.CharField(max_length=135, blank=True, verbose_name=_('专业复试备注'))
 
     # hr终面
-    hr_score = models.CharField(max_length=10, choices=HR_SCORE_TYPE, blank=True, verbose_name=r'HR复试综合等级',
-                                help_text=r'1-5分，极优秀:>=4.5，优秀:4-4.4，良好:3.5-3.9,一般:3-3.4,较差:<3')
-    hr_responsibility = models.CharField(max_length=10, choices=HR_SCORE_TYPE, blank=True, verbose_name=r'HR责任心')
+    hr_score = models.CharField(max_length=10, choices=HR_SCORE_TYPE, blank=True, verbose_name=_('HR复试综合等级'),
+                                help_text=_('1-5分，极优秀:>=4.5，优秀:4-4.4，良好:3.5-3.9,一般:3-3.4,较差:<3'))
+    hr_responsibility = models.CharField(max_length=10, choices=HR_SCORE_TYPE, blank=True, verbose_name=_('HR责任心'))
     hr_communication_ability = models.CharField(max_length=10, choices=HR_SCORE_TYPE, blank=True,
-                                                verbose_name=r'HR坦诚沟通')
-    hr_logic_ability = models.CharField(max_length=10, choices=HR_SCORE_TYPE, blank=True, verbose_name=r'HR逻辑思维')
-    hr_potential = models.CharField(max_length=10, choices=HR_SCORE_TYPE, blank=True, verbose_name=r'HR发展潜力')
-    hr_stability = models.CharField(max_length=10, choices=HR_SCORE_TYPE, blank=True, verbose_name=r'HR稳定性')
-    hr_advantage = models.TextField(max_length=1024, blank=True, verbose_name=r'优势')
-    hr_disadvantage = models.TextField(max_length=1024, blank=True, verbose_name=r'顾虑和不足')
+                                                verbose_name=_('HR坦诚沟通'))
+    hr_logic_ability = models.CharField(max_length=10, choices=HR_SCORE_TYPE, blank=True, verbose_name=_('HR逻辑思维'))
+    hr_potential = models.CharField(max_length=10, choices=HR_SCORE_TYPE, blank=True, verbose_name=_('HR发展潜力'))
+    hr_stability = models.CharField(max_length=10, choices=HR_SCORE_TYPE, blank=True, verbose_name=_('HR稳定性'))
+    hr_advantage = models.TextField(max_length=1024, blank=True, verbose_name=_('优势'))
+    hr_disadvantage = models.TextField(max_length=1024, blank=True, verbose_name=_('顾虑和不足'))
     hr_result = models.CharField(max_length=256,
-                                 choices=INTERVIEW_RESULT_TYPE, blank=True, verbose_name=r'HR复试结果')
+                                 choices=INTERVIEW_RESULT_TYPE, blank=True, verbose_name=_('HR复试结果'))
     hr_interviewer_user = models.ForeignKey(User, blank=True, null=True, on_delete=models.CASCADE,
-                                            related_name=r'hr_interviewer_user', verbose_name=r'HR面试官')
-    hr_remark = models.CharField(max_length=135, blank=True, verbose_name=r'HR复试备注')
+                                            related_name=r'hr_interviewer_user', verbose_name=_('HR面试官'))
+    hr_remark = models.CharField(max_length=135, blank=True, verbose_name=_('HR复试备注'))
 
-    creator = models.CharField(max_length=256, blank=True, verbose_name=r'候选人数据的创建人')
-    created_date = models.DateTimeField(auto_now_add=True, verbose_name=r'创建时间')
-    modified_date = models.DateTimeField(auto_now=True, null=True, blank=True, verbose_name=r'更新时间')
-    last_editor = models.CharField(max_length=256, blank=True, verbose_name=r'最后编辑者')
+    creator = models.CharField(max_length=256, blank=True, verbose_name=_('候选人数据的创建人'))
+    created_date = models.DateTimeField(auto_now_add=True, verbose_name=_('创建时间'))
+    modified_date = models.DateTimeField(auto_now=True, null=True, blank=True, verbose_name=_('更新时间'))
+    last_editor = models.CharField(max_length=256, blank=True, verbose_name=_('最后编辑者'))
 
     class Meta:
         db_table = r'candidate'
-        verbose_name = r'应聘者'
-        verbose_name_plural = r'应聘者'
+        verbose_name = _('应聘者')
+        verbose_name_plural = _('应聘者')
 
         permissions = [
             ("export", "Can export candidate list"),
--- jobs/models.py
+++ jobs/models.py
@@ -2,6 +2,7 @@ from django.db import models
 from django.contrib.auth.models import User
 from datetime import datetime
 from interview.models import DEGREE_TYPE
+from django.utils.translation import gettext_lazy as _
 
 JobTypes = [
     (0, "技术类"),
@@ -19,42 +20,48 @@ Cities = [
 # Create your models here.
 
 class Job(models.Model):
-    job_type = models.SmallIntegerField(blank=False, choices=JobTypes, verbose_name="职位类别")
-    job_name = models.CharField(blank=False, max_length=250, verbose_name="职位名称")
-    job_city = models.SmallIntegerField(blank=False, choices=Cities, verbose_name="工作地点")
-    job_responsibility = models.TextField(max_length=1024, verbose_name="职位职责")
-    job_requirement = models.TextField(max_length=1024, blank=False, verbose_name="职位要求")
+    # Translators: 职位实体的翻译
+    job_type = models.SmallIntegerField(blank=False, choices=JobTypes, verbose_name=_("职位类别"))
+    job_name = models.CharField(blank=False, max_length=250, verbose_name=_("职位名称"))
+    job_city = models.SmallIntegerField(blank=False, choices=Cities, verbose_name=_("工作地点"))
+    job_responsibility = models.TextField(max_length=1024, verbose_name=_("职位职责"))
+    job_requirement = models.TextField(max_length=1024, blank=False, verbose_name=_("职位要求"))
 
-    creator = models.ForeignKey(User, null=True, verbose_name="职位创建人", on_delete=models.SET_NULL)
-    created_date = models.DateTimeField(verbose_name="创建时间", default=datetime.now)
-    modified_date = models.DateTimeField(verbose_name="修改时间", default=datetime.now)
+    creator = models.ForeignKey(User, null=True, verbose_name=_("职位创建人"), on_delete=models.SET_NULL)
+    created_date = models.DateTimeField(verbose_name=_("创建时间"), default=datetime.now)
+    modified_date = models.DateTimeField(verbose_name=_("修改时间"), default=datetime.now)
+
+    class Meta:
+        verbose_name = _('职位')
+        verbose_name_plural = _('职位列表')
 
 
 class Resume(models.Model):
+    # Translators: 简历实体的翻译
     # 基本信息
-    username = models.CharField(max_length=135, verbose_name="姓名")
-    applicant = models.ForeignKey(User, verbose_name="申请人", null=True, blank=True, on_delete=models.SET_NULL)
-    city = models.CharField(max_length=135, verbose_name="城市")
-    phone = models.CharField(max_length=135, verbose_name="手机号码")
-    email = models.EmailField(max_length=135, blank=True, verbose_name="邮箱")
-    apply_position = models.CharField(max_length=135, blank=True, verbose_name="应聘职位")
-    born_address = models.CharField(max_length=135, blank=True, verbose_name="生源地")
-    gender = models.CharField(max_length=135, blank=True, verbose_name="性别")
+    username = models.CharField(max_length=135, verbose_name=_("姓名"))
+    applicant = models.ForeignKey(User, verbose_name=_("申请人"), null=True, blank=True, on_delete=models.SET_NULL)
+    city = models.CharField(max_length=135, verbose_name=_("城市"))
+    phone = models.CharField(max_length=135, verbose_name=_("手机号码"))
+    email = models.EmailField(max_length=135, blank=True, verbose_name=_("邮箱"))
+    apply_position = models.CharField(max_length=135, blank=True, verbose_name=_("应聘职位"))
+    born_address = models.CharField(max_length=135, blank=True, verbose_name=_("生源地"))
+    gender = models.CharField(max_length=135, blank=True, verbose_name=_("性别"))
 
     # 学校与学历信息
-    bachelor_school = models.CharField(max_length=135, blank=True, verbose_name="本科学校")
-    master_school = models.CharField(max_length=135, blank=True, verbose_name="研究生学校")
-    doctor_school = models.CharField(max_length=135, blank=True, verbose_name="博士生学校")
-    major = models.CharField(max_length=135, blank=True, verbose_name="专业")
-    degree = models.CharField(max_length=135, choices=DEGREE_TYPE, blank=True, verbose_name="学历")
-    created_date = models.DateTimeField(default=datetime.now, verbose_name="创建时间")
-    modified_date = models.DateTimeField(default=datetime.now, verbose_name="修改时间")
+    bachelor_school = models.CharField(max_length=135, blank=True, verbose_name=_("本科学校"))
+    master_school = models.CharField(max_length=135, blank=True, verbose_name=_("研究生学校"))
+    doctor_school = models.CharField(max_length=135, blank=True, verbose_name=_("博士生学校"))
+    major = models.CharField(max_length=135, blank=True, verbose_name=_("专业"))
+    degree = models.CharField(max_length=135, choices=DEGREE_TYPE, blank=True, verbose_name=_("学历"))
+    created_date = models.DateTimeField(default=datetime.now, verbose_name=_("创建时间"))
+    modified_date = models.DateTimeField(default=datetime.now, verbose_name=_("修改时间"))
 
     # 候选人自我介绍、工作经历、项目经历
-    candidate_introduction = models.TextField(max_length=1024, blank=True, verbose_name="自我介绍")
-    work_experience = models.TextField(max_length=1024, blank=True, verbose_name="工作经历")
-    project_experience = models.TextField(max_length=1024, blank=True, verbose_name="项目经历")
+    candidate_introduction = models.TextField(max_length=1024, blank=True, verbose_name=_("自我介绍"))
+    work_experience = models.TextField(max_length=1024, blank=True, verbose_name=_("工作经历"))
+    project_experience = models.TextField(max_length=1024, blank=True, verbose_name=_("项目经历"))
 
     class Meta:
-        verbose_name = "简历"
-        verbose_name_plural = "简历列表"
+        verbose_name = _("简历")
+        verbose_name_plural = _("简历列表")
